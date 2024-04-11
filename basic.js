50c1c3c2dafede4e09d9f12bf4b57685df704799
Shiv115singh <144952147+Shiv115singh@users.noreply.github.com>
2024-04-11 18:02:35 +0000

    basic.js

---
+++ basic.js
@@ -0,0 +1,9 @@
+const name="Shivam singh chauhan";
+let emailid="Sjadsjh@214"
+var adress="bbasdjhb"
+let jshbsd;
+distee="jadsbhbjh";
+//name="kndsj"
+emailid="sabhghv";
+console.log(name);
+console.table([name,emailid,adress,distee,jshbsd]);
---
+++ new.js
@@ -0,0 +1,4 @@
+int name = "shivam"
+console
+.l
+ole
